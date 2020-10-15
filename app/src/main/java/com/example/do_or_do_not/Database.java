24026d241abc8d07109ba9dc80473957a6ac0732
joseph1495 <josephibrahim1495@gmail.com>
2020-10-15 00:45:15 -0700

    Sprint1 done

--- app/src/main/java/com/example/do_or_do_not/Database.java
+++ app/src/main/java/com/example/do_or_do_not/Database.java
@@ -1,5 +1,8 @@
 package com.example.do_or_do_not;
+
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -14,22 +17,96 @@ class Database extends SQLiteOpenHelper {
     private static final String COLUMN_ID = "_id";
     private static final String COLUMN_TASKNAME = "task_name";
     private static final String COLUMN_TASKDATE = "task_date";
-    private static final String COLUMN_TASKDESC = "task_description";
-    private static final String COLUMN_TASKPRIORITY = "task_priority";
+    //private static final String COLUMN_TASKDESC = "task_description";
+    // private static final String COLUMN_TASKPRIORITY = "task_priority";
 
 
     public Database(@Nullable Context context) {
-        super(context, DATABASE_NAME, factory: null, DATABASE_VERSION);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
         this.context = context;
     }
+
     @Override
-    public void onCreate(SQLiteDatabase db){
-    String query = "CREATE TABLE " + TABLE_NAME + " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_TASKNAME + "TEXT, " + COLUMN_TASKDATE +  " TEXT, " + COLUMN_TASKDESC + "TEXT, " + COLUMN_TASKPRIORITY + " TEXT);";
-    db.execSQL(query);
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(
+                "CREATE TABLE " + TABLE_NAME + "(id INTEGER PRIMARY KEY, name TEXT, date TEXT, status INTEGER )"
+        );
+
     }
+
     @Override
-    public void onUpgrade(SQLiteDatabase db, int i, int i1){
-    db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME) ;
-    onCreate(db);
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
+
+    public boolean insertTask(String name, String date) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put("name", name);      //todo check here
+        contentValues.put("date", date);
+        contentValues.put("status", 0);
+        //  contentValues.put("COLUMN_TASKPRIORITY", priority);
+        db.insert(TABLE_NAME, null, contentValues);
+        return true;
+    }
+
+    public boolean updateTaskStatus(String id, Integer status) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("status", status);
+
+        db.update(TABLE_NAME, contentValues, "id = ? ", new String[]{id});
+        return true;
+    }
+
+    public boolean updateTask(String id, String name, String date) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put("name", name);
+        contentValues.put("date", date);
+        //contentValues.put("COLOMN_TASKDESC", description);
+
+        db.update(TABLE_NAME, contentValues, "id =? ", new String[]{id});
+        return true;
+    }
+
+    public boolean deleteTask(String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_NAME, "id = ? ", new String[]{id});
+        return true;
+    }
+
+    public Cursor getTodayTask() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME + " WHERE date(`date`) = date('now', 'localtime') order by id desc", null);
+        return res;
+
+    }
+
+    public Cursor getTomorrowTask() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME + " WHERE date(`date`) = date('now', '+1 day', 'localtime')  order by id desc", null);
+        return res;
+
+    }
+
+    public Cursor getSingleTask(String id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME + " WHERE id = '" + id + "' order by id desc", null);
+        return res;
+
+    }
+
+
+    public Cursor getUpcomingTask() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME + " WHERE date(date) > date('now', '+1 day', 'localtime') order by id desc", null);
+        return res;
+
+    }
+
+
 }
--- app/src/main/java/com/example/do_or_do_not/MainActivity.java
+++ app/src/main/java/com/example/do_or_do_not/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.do_or_do_not;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -7,50 +9,140 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.widget.NestedScrollView;
 
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    Database db;
+    LinearLayout empty;
+    NestedScrollView scrollView;
+    LinearLayout todayContainer, tomorrowContainer, otherContainer;
+    NoScrollListView taskListToday, taskListTomorrow, taskListUpcoming;
+    ArrayList<HashMap<String, String>> todayList = new ArrayList<HashMap<String, String>>();
+    ArrayList<HashMap<String, String>> tomorrowList = new ArrayList<HashMap<String, String>>();
+    ArrayList<HashMap<String, String>> upcomingList = new ArrayList<HashMap<String, String>>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        db = new Database(this);
+        empty = findViewById(R.id.empty);
+        scrollView = findViewById(R.id.scrollView);
+        todayContainer = findViewById(R.id.todayContainer);
+        tomorrowContainer = findViewById(R.id.tomorrowContainer);
+        otherContainer = findViewById(R.id.otherContainer);
+        taskListToday = findViewById(R.id.taskListToday);
+        taskListTomorrow = findViewById(R.id.taskListTomorrow);
+        taskListUpcoming = findViewById(R.id.taskListUpcoming);
+    }
+
+    public void openAddModifyTask(View view) {
+        startActivity(new Intent(this, AddModifyTask.class));
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        populateData();
+    }
+
+
+    public void populateData() {
+        db = new Database(this);
+
+        runOnUiThread(new Runnable() {
+            public void run() {
+                fetchDataFromDB();
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+
+    public void fetchDataFromDB() {
+        todayList.clear();
+        tomorrowList.clear();
+        upcomingList.clear();
+
+        Cursor today = db.getTodayTask();
+        Cursor tomorrow = db.getTomorrowTask();
+        Cursor upcoming = db.getUpcomingTask();
+
+        loadDataList(today, todayList);
+        loadDataList(tomorrow, tomorrowList);
+        loadDataList(upcoming, upcomingList);
+
+
+        if (todayList.isEmpty() && tomorrowList.isEmpty() && upcomingList.isEmpty()) {
+            empty.setVisibility(View.VISIBLE);
+            scrollView.setVisibility(View.GONE);
+        } else {
+            empty.setVisibility(View.GONE);
+            scrollView.setVisibility(View.VISIBLE);
+
+            if (todayList.isEmpty()) {
+                todayContainer.setVisibility(View.GONE);
+            } else {
+                todayContainer.setVisibility(View.VISIBLE);
+                loadListView(taskListToday, todayList);
+            }
+
+            if (tomorrowList.isEmpty()) {
+                tomorrowContainer.setVisibility(View.GONE);
+            } else {
+                tomorrowContainer.setVisibility(View.VISIBLE);
+                loadListView(taskListTomorrow, tomorrowList);
+            }
+
+            if (upcomingList.isEmpty()) {
+                otherContainer.setVisibility(View.GONE);
+            } else {
+                otherContainer.setVisibility(View.VISIBLE);
+                loadListView(taskListUpcoming, upcomingList);
+            }
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void loadDataList(Cursor cursor, ArrayList<HashMap<String, String>> dataList) {
+        if (cursor != null) {
+            cursor.moveToFirst();
+            while (cursor.isAfterLast() == false) {
+
+                HashMap<String, String> list = new HashMap<String, String>();
+                list.put("id", cursor.getString(0).toString());
+                list.put("task", cursor.getString(1).toString());
+                list.put("date", cursor.getString(2).toString());
+                list.put("status", cursor.getString(3).toString());
+                dataList.add(list);
+                cursor.moveToNext();
+            }
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void loadListView(NoScrollListView listView, final ArrayList<HashMap<String, String>> dataList) {
+        ListTaskAdapter adapter = new ListTaskAdapter(this, dataList, db);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Intent i = new Intent(MainActivity.this, AddModifyTask.class);
+                i.putExtra("isModify", true);
+                i.putExtra("id", dataList.get(+position).get("id"));
+                startActivity(i);
+            }
+        });
     }
 }
